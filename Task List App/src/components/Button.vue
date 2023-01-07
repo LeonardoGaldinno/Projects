fefad50d12adb154f220a28e404128ece728c3c0
LeonardoGaldinno <39771870+LeonardoGaldinno@users.noreply.github.com>
2023-01-07 12:05:14 -0300

    Create Button.vue

---
+++ Task List App/src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="onClick()"
+    v-bind:style="{background: color}" 
+    class="btn">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props:{
+        text: String,
+        color: String
+    },
+    methods:{
+        onClick() {
+            this.$emit('btn-click')
+        }
+    }
+}
+</script>
